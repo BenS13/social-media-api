56235d1df18b558f4d3eebbc82332e5fb706ffe8
Benjamin Sabatier <sabatierb@coventry.ac.uk>
2023-03-01 15:44:23 +0000

    Create model for likes

--- models/likes.js
+++ models/likes.js
@@ -1,1 +1,13 @@
 const db = require('../helpers/database');//import our database.js method to query database [query, values]
+
+exports.getLikes = async function getLikes(id){
+    //TODO
+};
+
+exports.addLike = async function addLike(id){
+    //TODO
+};
+
+exports.removeLike = async function removeLike(id){
+    //TODO
+};
