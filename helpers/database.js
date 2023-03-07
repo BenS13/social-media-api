1a26e9af9a3ae01262dd64de80f97176dd393b49
Benjamin Sabatier <sabatierb@coventry.ac.uk>
2023-03-07 14:41:05 +0000

    update comments

--- helpers/database.js
+++ helpers/database.js
@@ -2,7 +2,7 @@ const mysql = require('promise-mysql');
 const info = require('../config');
 
 //define async utitlity
-//run queury and then close connection CODE from week3Lab Doc
+//connect to db run queury and then close connection CODE from week3Lab Doc
 exports.run_query = async function run_query(query, values) {
     try {
       const connection = await mysql.createConnection(info.config);
